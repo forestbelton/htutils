7c4804201daa62f333bde7956767da1dd09e1c57
Forest Belton <fbelton@theore.tc>
2012-08-25 07:17:16 +0200

    Use Data.Array.Unboxed with a qualified name

--- htsim.hs
+++ htsim.hs
@@ -1,12 +1,12 @@
 import Control.Monad.State
-import Data.Array.Unboxed
+import qualified Data.Array.Unboxed as UA
 import Data.Map
 import Data.Word
 
 data Register  = A | B | C | D | E | F | G | H
                | I | J | K | L | M | N | O | P
-  deriving (Eq, Ord, Ix, Show)
-type Registers = UArray Register Word32
+  deriving (Eq, Ord, UA.Ix, Show)
+type Registers = UA.UArray Register Word32
 type Memory    = Map Word32 Word32
 type CPU       = State (Registers, Memory)
 
