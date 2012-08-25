de0117125e7e01100beb46cab8ad4c54d167cccf
Forest Belton <fbelton@theore.tc>
2012-08-25 07:05:30 +0200

    Move Registers definition under Register

--- htsim.hs
+++ htsim.hs
@@ -2,11 +2,10 @@ import Data.Array.Unboxed
 import Data.Map
 import Data.Word
 
-type Registers = UArray Register Word32
 data Register  = A | B | C | D | E | F | G | H
                | I | J | K | L | M | N | O | P
   deriving (Eq, Ord, Ix, Show)
-
+type Registers = UArray Register Word32
 type Memory    = Map Word32 Word32
 type CPU       = (Registers, Memory)
 
