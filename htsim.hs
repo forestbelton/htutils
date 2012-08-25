c30c234f067bd1d4cff585e31de277360193b429
Forest Belton <fbelton@theore.tc>
2012-08-25 07:29:01 +0200

    Add setMem function

--- htsim.hs
+++ htsim.hs
@@ -26,5 +26,11 @@ getMem :: Word32 -> CPU Word32
 getMem addr = do (_regs, mem) <- get
                  return $ findWithDefault 0 addr mem
 
+setMem :: Word32 -> Word32 -> CPU ()
+setMem addr val = do (regs, mem) <- get
+                     let mem' = insert addr val mem
+                     put (regs, mem')
+                     return ()
+
 main :: IO ()
 main = return ()
