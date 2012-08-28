e014feb32ced0366e1045cc1ab43f8d688467a69
Forest Belton <fbelton@theore.tc>
2012-08-28 02:09:18 +0200

    Add back the Operation enum for readability

--- htsim.hs
+++ htsim.hs
@@ -7,6 +7,11 @@ import Data.Word
 data Register  = A | B | C | D | E | F | G | H
                | I | J | K | L | M | N | O | P
   deriving (Eq, Ord, UA.Ix, Show, Enum)
+data Operation = OP_BIT_OR    | OP_BIT_AND  | OP_ADD     | OP_MUL
+               | OP_RESERVED0 | OP_SHIFTL   | OP_LT      | OP_EQ
+               | OP_GT        | OP_BIT_ANDN | OP_BIT_XOR | OP_SUB
+               | OP_BIT_XORN  | OP_SHIFTR   | OP_NEQ     | OP_RESERVED1
+  deriving Enum
 type Registers = UA.UArray Register Word32
 type Memory    = Map Word32 Word32
 type CPU       = State (Registers, Memory)
@@ -52,7 +57,7 @@ eval word = case mode of
         z      = toEnum $ fromIntegral $ extract word 24 4
         x      = getReg $ toEnum $ fromIntegral $ extract word 20 4
         y      = getReg $ toEnum $ fromIntegral $ extract word 16 4
-        f      = liftM2 $ getOp $ fromIntegral $ extract word 12 4
+        f      = liftM2 $ getOp $ toEnum $ fromIntegral $ extract word 12 4
         imm    = return $ sex $ extract word 0 12
         addM   = liftM2 (+)
         result = if swap then f x imm `addM` y else f x y `addM` imm
@@ -69,21 +74,21 @@ runCode = do
       do eval word
          runCode
 
-getOp :: Int -> (Word32 -> Word32 -> Word32)
-getOp 0 = \x y -> x .|. y
-getOp 1 = \x y -> x .&. y
-getOp 2 = \x y -> x + y
-getOp 3 = \x y -> x * y
-getOp 5 = \x y -> x * (2 ^ y)
---getOp 6 = \x y -> boolToReg (foo x < foo y)
---getOp 7 = \x y -> boolToReg (x == y)
---getOp 8 = \x y -> boolToReg (foo x > foo y)
-getOp 9 =  \x y -> x .&. (complement y)
---    OP_BIT_XOR -> \x y -> x `xor` y
---    OP_SUB -> \x y -> x - y
---    OP_BIT_XORN -> \x y -> x `xor` (complement y)
---    OP_SHIFTR -> \x y -> x `div` (2 ^ y)
---    OP_NEQ -> \x y -> boolToReg (x /= y)
+getOp :: Operation -> (Word32 -> Word32 -> Word32)
+getOp OP_BIT_OR   = \x y -> x .|. y
+getOp OP_BIT_AND  = \x y -> x .&. y
+getOp OP_ADD      = \x y -> x + y
+getOp OP_MUL      = \x y -> x * y
+getOp OP_SHIFTL   = \x y -> x * (2 ^ y)
+--getOp OP_LT       = \x y -> boolToReg (foo x < foo y)
+--getOp OP_EQ       = \x y -> boolToReg (x == y)
+--getOp OP_GT       = \x y -> boolToReg (foo x > foo y)
+getOp OP_BIT_ANDN =  \x y -> x .&. (complement y)
+getOp OP_BIT_XOR  = \x y -> x `xor` y
+getOp OP_SUB      = \x y -> x - y
+getOp OP_BIT_XORN = \x y -> x `xor` (complement y)
+getOp OP_SHIFTR   = \x y -> x `div` (2 ^ y)
+--getOp OP_NEQ      = \x y -> boolToReg (x /= y)
 
 main :: IO ()
 main = return ()
