c3db59a7c0f5e029c30e5dc5559560a53c84f498
Forest Belton <fbelton@theore.tc>
2014-01-26 00:52:28 -0800

    Use IntMap over Map (requires unsafeCoerce)

--- src/htsim/Arch.hs
+++ src/htsim/Arch.hs
@@ -1,16 +1,17 @@
 module Arch where
 
 import Data.Array.Unboxed
-import Data.Map hiding ((!))
+import Data.IntMap hiding ((!))
 import Data.IORef
 import Data.Word
+import Unsafe.Coerce
 
 data Register = A | B | C | D | E | F | G | H
               | I | J | K | L | M | N | O | P
   deriving (Eq, Ord, Ix, Show, Enum)
 
 type Registers = UArray Register Word32
-type Memory    = Map Word32 Word32
+type Memory    = IntMap Word32
 type CPU       = IORef (Registers, Memory)
 
 getReg :: Register -> CPU -> IO Word32
@@ -26,10 +27,10 @@ setReg reg val cpu = modifyIORef' cpu $ \(regs, mem) -> (regs // [(reg, val)], m
 getMem :: Word32 -> CPU -> IO Word32
 getMem addr cpu = do
   (_, mem) <- readIORef cpu
-  return $ findWithDefault 0 addr mem
+  return $ findWithDefault 0 (unsafeCoerce addr) mem
 
 setMem :: Word32 -> Word32 -> CPU -> IO ()
-setMem addr val cpu = modifyIORef' cpu $ \(regs, mem) -> (regs, insert addr val mem)
+setMem addr val cpu = modifyIORef' cpu $ \(regs, mem) -> (regs, insert (unsafeCoerce addr) val mem)
 
 initCPU :: IO CPU
 initCPU = newIORef (regs, mem)
